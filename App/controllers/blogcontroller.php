48724b91349b68fd68c2502088bee4f74e4d5df5
NguyenDuc2010 <ryobaby03@gmail.com>
2025-04-09 03:20:29 +0700

    duc views = productdetailss + blog + blogdetails

--- App/controllers/blogcontroller.php
+++ App/controllers/blogcontroller.php
@@ -31,4 +31,33 @@ class Blogcontroller
         // Hiển thị view với dữ liệu products
         include __DIR__ . '/../views/blog/blog_index.php';
     }
+
+    public function show()
+    {
+        if (!isset($_GET['id'])) {
+            die("Thiếu ID bài viết");
+        }
+
+        $id = $_GET['id'];
+        $url = "http://localhost:8080/web_bandienthoai_mysql_php/api/blog/show/" . $id;
+
+        try {
+            $json = file_get_contents($url);
+            if ($json === false) {
+                throw new Exception('Không thể lấy dữ liệu từ API');
+            }
+
+            $response = json_decode($json, true);
+            if (empty($response) || !isset($response['id'])) {
+                throw new Exception('Không tìm thấy bài viết hoặc dữ liệu không hợp lệ.');
+            }
+
+            $blog = $response; // Gán dữ liệu vào biến $blog
+        } catch (Exception $e) {
+            die("Lỗi: " . $e->getMessage());
+        }
+
+        // Hiển thị view chi tiết bài viết
+        include __DIR__ . '/../views/blog/blogdetails.php';
+    }
 }
--- App/views/blog/blogdetails.php
+++ App/views/blog/blogdetails.php
@@ -16,7 +16,7 @@
                     <div class="row">
                         <div class="col-sm-6">
                             <div class="page-header-content">
-                                <h2 class="page-header-title">Blog</h2>
+                                <h2 class="page-header-title">Blog Details</h2>
                                 <ol class="breadcrumb">
                                     <li class="breadcrumb-item"><a href="index.html">Home //</a></li>
                                     <li class="breadcrumb-item active" aria-current="page">Blog</li>
@@ -33,121 +33,26 @@
                 </div>
             </div>
             <!--== End Page Header Area Wrapper ==-->
-
-            <!--== Start Blog Detail Area Wrapper ==-->
-            <div class="blog-detail-area section-space">
-                <div class="container">
-                    <div class="blog-detail">
-                        <img class="blog-detail-img w-100 mb-7" src="<?= BASE_URL ?>/public/images/blog/d4.jpg"
-                            width="1170" height="500" alt="Image">
-                        <div class="blog-detail-meta">
-                            <a class="blog-detail-post-date" href="blog.html"><i class="icon-calendar"></i> March 13,
-                                2022</a>
-                            <a class="blog-detail-post-views" href="blog.html"><i class="icon-eye"></i> 290 Views</a>
+            <div class="container">
+                <?php if (!empty($blog)): ?>
+                    <div class="blog-details">
+                        <h1 class="blog-title"><?= htmlspecialchars($blog['title']) ?></h1>
+                        <div class="blog-thumbnail">
+                            <img src="<?= htmlspecialchars($blog['thumbnail']) ?>" alt="<?= htmlspecialchars($blog['title']) ?>" class="img-fluid">
                         </div>
-                        <h2 class="blog-detail-title">Lorem ipsum dolor sit amet, consectetur adip elit, sed do eiusmod
-                            tempor.
-                        </h2>
-                        <p class="mb-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                            incididunt ut laborelio et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                            exercitation ullamco laboris nisi ut aliql ex ea commodo consequat.
-                            Duis aute irure dolor in reprehender in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                            mollit anim id est laborum. Sed ut perspiciatis
-                            unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
-                            aperiam, eaque ipsa quae ab illo inventore veritatis et quasi archite beatae vitae dicta
-                            sunt explicabo. Nemo enim ipsam voluptatem.</p>
-                        <blockquote class="blog-detail-blockquote">
-                            <p class="desc">Lorem ipsum dolor sit amet, consectetur adipi elit sed do eiusmod tempor sed
-                                do eiusmod amero incididunt.</p>
-                            <img class="quote-icon" src="<?= BASE_URL ?>/public/images/icons/quote.png" width="127"
-                                height="93" alt="Icon">
-                        </blockquote>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                            laborelio et dolore magna aliqua. <span class="text-primary text-decoration-underline">Ut
-                                enim ad minim veniam quis nostrud exercitation.</span> ullamco laboris nisi ut aliql ex
-                            ea commodo consequat. Duis aute irure dolor in reprehender in voluptate velit esse cillum
-                            dolore eu fugiat nulla pariatur. Excepteur
-                            sint occaecat cupidatat non proident.</p>
-                        <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                            incididunt ut laborelio et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                            exercitation ullamco laboris nisi ut aliql ex ea commodo consequat.
-                            Duis aute irure dolor in reprehender in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident.</p>
-                        <div class="blog-detail-tag-social">
-                            <div class="blog-detail-tag">
-                                <a class="tag-item" href="blog.html"><i class="icon-close"></i> Electronic</a>
-                                <a class="tag-item" href="blog.html"><i class="icon-close"></i> Home</a>
-                                <a class="tag-item" href="blog.html"><i class="icon-close"></i> Smart</a>
-                            </div>
-                            <div class="blog-detail-social">
-                                <span>Share On:</span>
-                                <a class="blog-detail-social-item linkedin-item" href="https://www.linkedin.com/"
-                                    target="_blank" rel="noopener"><i class="fa fa-linkedin"></i></a>
-                                <a class="blog-detail-social-item rss-item" href="https://www.rss.com/" target="_blank"
-                                    rel="noopener"><i class="fa fa-rss"></i></a>
-                                <a class="blog-detail-social-item dribbble-item" href="https://www.dribbble.com/"
-                                    target="_blank" rel="noopener"><i class="fa fa-dribbble"></i></a>
-                            </div>
+                        <div class="blog-meta">
+                            <p><strong>Tác giả:</strong> <?= htmlspecialchars($blog['author']) ?></p>
+                            <p><strong>Ngày tạo:</strong> <?= htmlspecialchars($blog['created_at']) ?></p>
                         </div>
-                        <div class="comment-area">
-                            <h3 class="comment-area-title">Comments</h3>
-                            <!--== Start Single Comment Item ==-->
-                            <div class="single-comment">
-                                <div class="single-comment-thumb">
-                                    <img src="<?= BASE_URL ?>/public/images/blog/c1.png" alt="Images" width="120"
-                                        height="120">
-                                </div>
-                                <div class="single-comment-content">
-                                    <h4 class="single-comment-name">Jakobe Snell</h4>
-                                    <span class="single-comment-date">24 July, 2022</span>
-                                    <p class="desc">Lorem ipsum dolor sit amet, consectet adipisicing elit, sed do
-                                        eiuslo tempor incididunt ut labo et dolore magna aliqua. Ut enim ad minim
-                                        veniam, quis nostrud exercitation ullamco</p>
-                                </div>
-                                <button type="button" class="reply-icon"><i class="fa fa-mail-reply-all"></i>
-                                    Reply</button>
-                            </div>
-                            <!--== End Single Comment Item ==-->
-                        </div>
-                        <div class="comment-form-area">
-                            <form class="comment-form" id="comment-form"
-                                action="https://whizthemes.com/mail-php/raju/arden/mail.php" method="post">
-                                <h3 class="comment-form-title"> Leave a Comments</h3>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="form-input-item">
-                                            <input class="form-control" type="text" name="con_name" id="name"
-                                                placeholder="Your Name">
-                                            <i class="icon fa fa-user"></i>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-input-item">
-                                            <input class="form-control" type="email" name="con_email" id="mail"
-                                                placeholder="Enter Your Mail">
-                                            <i class="icon fa fa-envelope"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-input-item">
-                                    <input class="form-control" type="text" name="con_subject" id="subject"
-                                        placeholder="Your Subject">
-                                    <i class="icon fa fa-phone-square"></i>
-                                </div>
-                                <div class="form-input-item">
-                                    <textarea class="form-control" name="con_message" id="message"
-                                        placeholder="Your Message"></textarea>
-                                </div>
-                                <button class="btn btn-theme btn-submit mt-2" type="submit">Post Comment <i
-                                        class="icon fa fa-arrow-right"></i></button>
-                            </form>
-                            <!--== End Contact Form ==-->
+                        <div class="blog-content">
+                            <p><?= nl2br(htmlspecialchars($blog['content'])) ?></p>
                         </div>
                     </div>
-                </div>
+                <?php else: ?>
+                    <p>Bài viết không tồn tại hoặc có lỗi xảy ra.</p>
+                <?php endif; ?>
             </div>
-            <!--== End Blog Detail Area Wrapper ==-->
+
         </main>
     </div>
 </body>
